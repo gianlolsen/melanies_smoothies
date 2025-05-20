59e0dd31e15e65b3fcd0688e005bd68e7adfd493
gianlolsen <gianlolsen@gmail.com>
2025-05-20 14:03:00 -0700

    Update streamlit_app.py
    updating app to connect to smoothiefruit for nutrition facts to the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 #import a snowpark column function as well for the form
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -50,3 +51,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
