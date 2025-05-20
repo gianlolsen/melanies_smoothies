4e501286e1cba1de6000b52a0f72d0f7f336e060
gianlolsen <gianlolsen@gmail.com>
2025-05-20 15:37:58 -0700

    Update streamlit_app.py
    Fix bug causing quotes on names

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ if ingredients_list:
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+            values ('{ingredients_string.strip()}', '{name_on_order.strip()}')
 
     #st.write(my_insert_stmt)
     #st.stop()
